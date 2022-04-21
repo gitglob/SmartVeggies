92524d1580eebc089f3ba7d568b7a51981a7d5df
A01748293 <A01748293@itesm.mx>
2022-04-21 11:48:44 +0200

    Almost done
    Commented the WiFi stuff so we can do individual component testing. We will split the code into 2 ESPs one for pump and moisture and other one for everything else.

--- GreenHouse/GreenHouse.ino
+++ GreenHouse/GreenHouse.ino
@@ -5,7 +5,7 @@
 // Replace with your network details
 const char* ssid = "Memo";
 const char* password = "12345678";
-WiFiClient client;
+//WiFiClient client;
 
 unsigned long channelID = 1710181; //your channel
 const char * myWriteAPIKey = "1FZ6IC3WPT4UIVLO"; // your WRITE API key
@@ -25,7 +25,7 @@ int waterPercentage = 0;
 #define AnalogInput A0          //ESP only has one analog input gonna be used for moisture and water level
 
 //Moisture sensor
-#define ReadMoist 12     //D6  GPIO12  Turns on and off the moisture sensor
+#define ReadMoist 13     //D7  GPIO13  Turns on and off the moisture sensor
 int MoistLevel = 0;
 int MoistPercentage = 0;
 #define WetThres 450
@@ -61,15 +61,16 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+  //WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
+  /*
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+  */
 
 
 
@@ -78,69 +79,79 @@ void setup() {
 //-----------------------------------------------------------------------Void Loop------------------------------------------------
 void loop() {
   //WiFi things
-  ThingSpeak.begin(client);
-  if (client.connect(server, 80)) {
+  /*
+    ThingSpeak.begin(client);
+    if (client.connect(server, 80)) {
 
     // Measure Signal Strength (RSSI) of Wi-Fi connection
     long rssi = WiFi.RSSI();
     Serial.print("RSSI: ");
     Serial.println(rssi);
     ThingSpeak.setField(5, rssi);
+  */
+  //Read the sensors
+  //Reading the humidity and temperature
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-    //Read the sensors
-    //Reading the humidity and temperature
-    delay(2000);
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-
-    if (isnan(h) || isnan(t)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
-    Serial.println((String)"Humidity: " + h + " %\t");
-    Serial.println((String)"Temperature: " + t + 0xC2 + 0xB0 + "C");
-
-    delay(5000);
-
-    //Reading the water level
-    ReadWaterLevel();
-    Serial.println((String)"Water Level is " + waterPercentage + "%");
-
-    delay(5000);
-
-    //Reading the moisture
-    ReadMoisture();
-    Serial.println((String)"Moisture is " + MoistLevel + "%");
-
-
-    ThingSpeak.setField(1, t);    //Temperature
-    ThingSpeak.setField(2, h);    //Humidity
-    ThingSpeak.setField(3, MoistLevel);    //Soil Moisture
-    ThingSpeak.setField(4, waterPercentage);    //Water deposit
-
-
-    ThingSpeak.writeFields(channelID, myWriteAPIKey);
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
   }
-  client.stop();
-
-  // wait and then post again
-  delay(20000);
-
-  //  Serial.println("Hello the code is running now :D");
-
-
+  Serial.println((String)"Humidity: " + h + " %\t");
+  Serial.println((String)"Temperature: " + t + 0xC2 + 0xB0 + "C");
 
   delay(5000);
-  Serial.println("Opening window");
-  OpenWindow(7);
-  Serial.println("Closing window");
-  delay(5000);
 
-  Serial.println("Turning ON the water pump");
-  digitalWrite(pump, HIGH);
+  //Reading the water level
+  ReadWaterLevel();
+  Serial.println((String)"Water Level is " + waterPercentage + "%");
+
   delay(10000);
-  Serial.println("Turning OFF the water pump");
-  digitalWrite(pump, LOW);
+
+  //Reading the moisture
+  ReadMoisture();
+  Serial.println((String)"Moisture is " + MoistLevel + "%");
+
+  /*
+      ThingSpeak.setField(1, t);    //Temperature
+      ThingSpeak.setField(2, h);    //Humidity
+      ThingSpeak.setField(3, MoistLevel);    //Soil Moisture
+      ThingSpeak.setField(4, waterPercentage);    //Water deposit
+
+
+      ThingSpeak.writeFields(channelID, myWriteAPIKey);
+    }
+    client.stop();
+
+    // wait and then post again
+    delay(20000);
+  */
+  //Logic and greenhouse functionality---------------------------------------------------------------------------------------
+  //Water deposit
+  if (waterPercentage <= 20) {
+    Serial.println("Please refill the water deposit");
+    delay(5000);
+  }
+
+  //Soil moisture
+  if (MoistLevel >= 1000) {
+    PumpWater(10);
+    delay(5000);
+  }
+
+  //Humidity and temperature
+  if (t >= 26 || h >= 35) {
+    Serial.println("Opening window");
+    OpenWindow(7);
+    Serial.println("Closing window");
+    delay(5000);
+  }
+  
+  delay(5000);
+
+
 
 
 
@@ -152,6 +163,7 @@ void loop() {
 //Reads the moisture sensor 100 times and gets the average to ensure a good reading
 int ReadMoisture() {
   digitalWrite(ReadMoist, HIGH);
+  delay(10000);
   for (int i = 0; i <= 100; i++)
   {
     MoistLevel = MoistLevel + analogRead(AnalogInput);
@@ -178,4 +190,15 @@ void OpenWindow(int opTime) {
   window.write(180);  //Open the window
   delay(opTime * 1000);
   window.write(80);  //Close the window
+}
+
+//Activates the pump for a certain amount of time
+//Recieves the amount of seconds you want to activate it (parameter given in seconds)
+void PumpWater(int opTimeP) {
+  Serial.println("Turning ON the water pump");
+  digitalWrite(pump, HIGH);
+  delay(opTimeP * 1000);
+  Serial.println("Turning OFF the water pump");
+  digitalWrite(pump, LOW);
+
 }
